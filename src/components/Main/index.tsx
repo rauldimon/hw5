7b654ec6aca99df52c02a40d6f512fa1cf5f2539
rauldimon <rauldimon2@gmail.com>
2020-02-28 13:10:35 +0200

    hw5

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Google from './google.png'
+import Input from "./Input"
+import Search from "./Search"
+//import style from "./style.module.css"
+
+class Main extends React.Component<any> {
+  render() {
+    return (
+      <div>
+        <img alt="google" src={Google} />
+        <Input />
+        <Search name="Поиск в Google"/>
+        <Search name="Мне повезёт!"/>
+      </div>
+    )
+  }
+}
+
+export default Main
